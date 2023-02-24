87fedcac5d5bea230f6ded10f44a9d6ebeaa22c3
abdoulfataoh <abdoulfataoh@gmail.com>
2023-02-24 22:06:40 +0000

    remove cleaner function

--- src/preprocessors/__init__.py
+++ src/preprocessors/__init__.py
@@ -1,13 +1,11 @@
 # coding: utf-8
 
 from src.preprocessors.functions import tokenizer
-from src.preprocessors.functions import cleaner
 from src.preprocessors.functions import avg_words_vectors
 from src.preprocessors.functions import vectorize_messages
 
 __all__ = [
     'tokenizer',
-    'cleaner',
     'avg_words_vectors',
     'vectorize_messages',
 ]
