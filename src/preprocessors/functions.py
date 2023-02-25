94635afa5ecbffc9ab9fbab6399c1d1a05c69604
abdoulfataoh <abdoulfataoh@gmail.com>
2023-02-25 10:32:49 +0000

    fix avg_words_vectors

--- src/preprocessors/functions.py
+++ src/preprocessors/functions.py
@@ -4,7 +4,7 @@ import logging
 from typing import List
 
 import spacy
-from gensim.models import word2vec
+from src.models import Word2vec
 import numpy as np
 
 logger = logging.getLogger(__name__)
@@ -18,7 +18,7 @@ def tokenizer(input: str):
 
 
 def avg_words_vectors(
-    word2vec: word2vec,
+    word2vec: Word2vec,
     words_list: List[str],
     vector_size: int
 ) -> np.ndarray:
@@ -26,7 +26,7 @@ def avg_words_vectors(
     words_vectors_sum = np.zeros(vector_size, dtype=np.float32)
     words_vectors_count = 0
     for word in words_list:
-        word_vec = word2vec.wv[word]
+        word_vec = word2vec.to_vectors([word])[0]
         words_vectors_count = words_vectors_count + 1
         words_vectors_sum = np.add(words_vectors_sum, word_vec)
     words_vectors_avg = np.divide(words_vectors_sum, words_vectors_count)
